78e0e73fd0fa3d03ecae61cea5b7506a4668f2ca
Ayano <wtiawtiawmps@gmail.com>
2021-10-25 10:22:56 +0000

    課題 PHP/Laravel 10

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>課題10-1</title>
+    </head>
+    <body>
+        <h1>提出します</h1>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>課題10-2</title>
+    </head>
+    <body>
+        <h5>よろしくお願いします</h5>
+    </body>
+</html>
