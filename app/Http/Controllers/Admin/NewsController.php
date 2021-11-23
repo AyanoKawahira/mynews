84dd34c48a08d51997990c27760bfb3220195c96
Ayano <wtiawtiawmps@gmail.com>
2021-11-23 04:25:25 +0000

    PHP/Laraval 13 課題

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,9 +7,14 @@ use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-    //　以下を追記
     public function add()
     {
+        // dd(__METHOD__);
         return view('admin.news.create');
+    }
+    
+    public function create(Request $request)
+    {
+        return redirect('admin/news/create');
     }
 }
